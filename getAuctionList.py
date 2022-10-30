a44e9173103318fcc46dcc04878565aaa60dcb66
choseungyoon <cho31250@gmail.com>
2022-10-30 16:00:55 +0900

    Change chromedriver version

--- getAuctionList.py
+++ getAuctionList.py
@@ -38,7 +38,7 @@ class GetAuctionInfo():
         # 창 숨기는 옵션 추가
         options.add_argument("start-maximized")
         options.add_argument("lang=ko_KR")
-        # options.add_argument('headless')
+        options.add_argument('headless')
         options.add_argument('window-size=1920x1080')
         options.add_argument("disable-gpu")
         options.add_argument("--no-sandbox")
