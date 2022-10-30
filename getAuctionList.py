9a4e9affc7948b7b43391fb4409df68da44ffbe3
choseungyoon <cho31250@gmail.com>
2022-10-30 16:36:15 +0900

    Change chrome to firefox

--- getAuctionList.py
+++ getAuctionList.py
@@ -33,16 +33,25 @@ class GetAuctionInfo():
 
     def setup_method(self, method):
         # 옵션 생성
-        options = webdriver.ChromeOptions()
+        #options = webdriver.ChromeOptions()
+        options = webdriver.FirefoxOptions()
 
         # 창 숨기는 옵션 추가
-        options.add_argument("start-maximized")
-        options.add_argument("lang=ko_KR")
-        options.add_argument('headless')
-        options.add_argument('window-size=1920x1080')
-        options.add_argument("disable-gpu")
-        options.add_argument("--no-sandbox")
-        self.driver = webdriver.Chrome('chromedriver', options=options)
+        # options.add_argument("start-maximized")
+        # options.add_argument("lang=ko_KR")
+        # options.add_argument('headless')
+        # options.add_argument('window-size=1920x1080')
+        # options.add_argument("disable-gpu")
+        # options.add_argument("--no-sandbox")
+
+        options.headless = True
+
+        #self.driver = webdriver.Chrome('chromedriver', options=options)
+        # self.driver = webdriver.Firefox(
+        #     executable_path='/Users/seungyuncho/Project/crawler_auction_info/geckodriver', options=options)
+        self.driver = webdriver.Firefox(
+            executable_path='geckodriver', options=options)
+
         self.driver.implicitly_wait(3)
 
         self.vars = {}
