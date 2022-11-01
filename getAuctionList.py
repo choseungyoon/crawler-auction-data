990171a8742a8064995c2912bb7cb0c7b339ad32
choseungyoon <cho31250@gmail.com>
2022-11-01 19:02:33 +0900

    Reset to firefox

--- getAuctionList.py
+++ getAuctionList.py
@@ -33,21 +33,21 @@ class GetAuctionInfo():
 
     def setup_method(self, method):
         # 옵션 생성
-        options = webdriver.ChromeOptions()
-        #options = webdriver.FirefoxOptions()
+        #options = webdriver.ChromeOptions()
+        options = webdriver.FirefoxOptions()
 
         # 창 숨기는 옵션 추가
-        options.add_argument("start-maximized")
-        options.add_argument("lang=ko_KR")
-        options.add_argument('headless')
-        options.add_argument('window-size=1920x1080')
-        options.add_argument("disable-gpu")
-        options.add_argument("--no-sandbox")
+        # options.add_argument("start-maximized")
+        # options.add_argument("lang=ko_KR")
+        # options.add_argument('headless')
+        # options.add_argument('window-size=1920x1080')
+        # options.add_argument("disable-gpu")
+        # options.add_argument("--no-sandbox")
 
-        # options.add_argument("--headless")
-        self.driver = webdriver.Chrome('chromedriver', options=options)
-        # self.driver = webdriver.Firefox(
-        #     executable_path='mac/geckodriver', options=options)
+        options.add_argument("--headless")
+        #self.driver = webdriver.Chrome('chromedriver', options=options)
+        self.driver = webdriver.Firefox(
+            executable_path='mac/geckodriver', options=options)
         #self.driver = webdriver.Firefox(executable_path='geckodriver', options=options)
 
         # self.driver.implicitly_wait(3)
