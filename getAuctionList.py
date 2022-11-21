176ed980c752d660292db7465fc9c9a3a4a3469b
choseungyoon <cho31250@gmail.com>
2022-11-21 22:29:07 +0900

    Change to chrome

--- getAuctionList.py
+++ getAuctionList.py
@@ -35,10 +35,10 @@ class GetAuctionInfo():
 
     def setup_method(self, method):
         # 옵션 생성
-        # options = webdriver.ChromeOptions()
-        options = webdriver.FirefoxOptions()
+        options = webdriver.ChromeOptions()
+        # options = webdriver.FirefoxOptions()
 
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         # open Browser in maximized mode
         options.add_argument("start-maximized")
         options.add_argument("disable-infobars")  # disabling infobars
@@ -47,9 +47,9 @@ class GetAuctionInfo():
         # overcome limited resource problems
         options.add_argument("--disable-dev-shm-usage")
 
-        # self.driver = webdriver.Chrome('chromedriver', options=options)
-        self.driver = webdriver.Firefox(
-            executable_path='linux/geckodriver', options=options)
+        self.driver = webdriver.Chrome('chromedriver', options=options)
+        # self.driver = webdriver.Firefox(
+        #    executable_path='linux/geckodriver', options=options)
 
         # self.driver.implicitly_wait(3)
 
