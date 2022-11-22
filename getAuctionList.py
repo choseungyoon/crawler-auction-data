e019e248a1f7044da5fd2b63c629a8fee638bc5d
choseungyoon <cho31250@gmail.com>
2022-11-23 07:48:21 +0900

    Merge

--- getAuctionList.py
+++ getAuctionList.py
@@ -35,8 +35,8 @@ class GetAuctionInfo():
 
     def setup_method(self, method):
         # 옵션 생성
-        # options = webdriver.ChromeOptions()
-        options = webdriver.FirefoxOptions()
+        options = webdriver.ChromeOptions()
+        #options = webdriver.FirefoxOptions()
 
         options.add_argument("--headless")
         # open Browser in maximized mode
@@ -47,8 +47,8 @@ class GetAuctionInfo():
         # overcome limited resource problems
         options.add_argument("--disable-dev-shm-usage")
 
-        #self.driver = webdriver.Chrome('chromedriver', options=options)
-        self.driver = webdriver.Firefox(executable_path='linux/geckodriver', options=options)
+        self.driver = webdriver.Chrome('chromedriver', options=options)
+        #self.driver = webdriver.Firefox(executable_path='linux/geckodriver', options=options)
 
         # self.driver.implicitly_wait(3)
 
