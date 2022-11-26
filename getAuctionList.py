4ac666d3233497ffc569020071d102bb9ff04f75
choseungyoon <cho31250@gmail.com>
2022-11-27 08:31:45 +0900

    Set linux

--- getAuctionList.py
+++ getAuctionList.py
@@ -47,7 +47,7 @@ class GetAuctionInfo():
         # overcome limited resource problems
         options.add_argument("--disable-dev-shm-usage")
 
-        self.driver = webdriver.Chrome('mac/chromedriver', options=options)
+        self.driver = webdriver.Chrome('chromedriver', options=options)
         # self.driver = webdriver.Firefox(executable_path='linux/geckodriver', options=options)
 
         # self.driver.implicitly_wait(3)
